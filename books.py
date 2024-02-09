07428e2e07e0b417a1222578e35da996b4e26897
jltavera <jl.tavera@uniandes.edu.co>
2024-02-09 06:59:42 -0500

    Path parameters

--- books.py
+++ books.py
@@ -12,6 +12,12 @@ BOOKS = [
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-async def first_api():
-    return {'message': 'Hola, Tavera!'}
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS: 
+        if book.get('title').casefold() == book_title.casefold() :
+            return book
