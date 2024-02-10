fa02fd45cff0d17f1e7b9496b63b8e0723b886c6
jltavera <jl.tavera@uniandes.edu.co>
2024-02-10 10:55:03 -0500

    Pydantic One

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -19,6 +20,15 @@ class Book:
         self.published_date = published_date
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
+    published_date: int
+
+
 
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
@@ -33,8 +43,14 @@ async def read_all_books():
     return BOOKS
 
 @app.post("/create-book")
-async def create_book(book_request= Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book:Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+
+    return book
 
 
 
