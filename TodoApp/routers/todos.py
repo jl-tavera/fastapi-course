82feac94404758a1794a379cfc7fd4dc610cbff6
jltavera <jl.tavera@uniandes.edu.co>
2024-02-27 08:05:26 -0500

    Authorize todos

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Path
 from models import Todos
 from database import  SessionLocal
 from pydantic import BaseModel, Field
+from .auth import get_current_user
 
 
 router = APIRouter()
@@ -17,6 +18,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
@@ -26,8 +28,9 @@ class TodoRequest(BaseModel):
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
+async def read_all(user: user_dependency,
+                db: db_dependency):
+    return db.query(Todos).filter(Todos.owner_id == user['id']).all()
 
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int =  Path(gt = 0)):
@@ -37,8 +40,14 @@ async def read_todo(db: db_dependency, todo_id: int =  Path(gt = 0)):
     raise HTTPException(status_code=404, detail="Todo not found")
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(todo_request: TodoRequest, db: db_dependency):
-    todo_model = Todos(**todo_request.dict())
+async def create_todo(todo_request: TodoRequest,
+                      user: user_dependency,
+                       db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
+    todo_model = Todos(**todo_request.model_dump(), owner_id=user['id'])
+
     db.add(todo_model)
     db.commit()
 
