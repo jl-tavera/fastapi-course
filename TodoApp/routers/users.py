67c503300003339b6da02bf32b7528971cfd5d06
jltavera <jl.tavera@uniandes.edu.co>
2024-02-29 12:27:24 -0500

    Root package

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -1,8 +1,8 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status, Path
-from models import Todos, Users
-from database import  SessionLocal
+from ..models import Todos, Users
+from ..database import  SessionLocal
 from pydantic import BaseModel, Field
 from .auth import get_current_user
 from passlib.context import CryptContext
